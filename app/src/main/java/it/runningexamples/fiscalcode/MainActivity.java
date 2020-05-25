bb97c44eb0395396514e3fa080112251506b19da
MicheleSalvatori <misax.117@gmail.com>
2020-05-25 16:26:30 +0200

    Initial commit

---
+++ app/src/main/java/it/runningexamples/fiscalcode/MainActivity.java
@@ -0,0 +1,14 @@
+package it.runningexamples.fiscalcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
