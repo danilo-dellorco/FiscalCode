1d2d48a7121439cfabbd2b2d17939c8094c32169
danilo-dellorco <danilodellorcolp@gmail.com>
2020-05-25 18:46:36 +0200

    temino

--- app/src/main/java/it/runningexamples/fiscalcode/MainActivity.java
+++ app/src/main/java/it/runningexamples/fiscalcode/MainActivity.java
@@ -3,12 +3,15 @@ package it.runningexamples.fiscalcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.DarkTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner sp = findViewById(R.id.spComuni);
     }
 }
