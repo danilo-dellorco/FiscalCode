0b8afbc9584256442736ac6db9ba22b4673d10e4
MicheleSalvatori <misax.117@gmail.com>
2020-05-25 17:56:58 +0200

    Comune.java Parser.java

---
+++ app/src/main/java/it/runningexamples/fiscalcode/Comune.java
@@ -0,0 +1,36 @@
+package it.runningexamples.fiscalcode;
+
+public class Comune {
+    private String name;
+    private String prov;
+    private String code;
+
+    public Comune(String name, String prov, String code) {
+        this.name = name;
+        this.prov = prov;
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProv() {
+        return prov;
+    }
+
+    public void setProv(String prov) {
+        this.prov = prov;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ app/src/main/java/it/runningexamples/fiscalcode/Parser.java
@@ -0,0 +1,7 @@
+package it.runningexamples.fiscalcode;
+
+public class Parser {
+
+    private static String DB_NAME = "comuni.csv";
+
+}
