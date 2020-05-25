3d164f02c98092589f6ce3e0079890093b91c8ac
MicheleSalvatori <misax.117@gmail.com>
2020-05-25 16:37:43 +0200

    creata classe CodiceFiscale

--- app/src/main/java/it/runningexamples/fiscalcode/CodiceFiscale.java
+++ app/src/main/java/it/runningexamples/fiscalcode/CodiceFiscale.java
@@ -1,8 +1,18 @@
 package it.runningexamples.fiscalcode;
 
 public class CodiceFiscale {
+    private String nome, cognome, comuneNascita;
+    private int giornoNascita, meseNascita, annoNascita;
+    private char sesso;
 
-    public CodiceFiscale(){
-        //
+    public CodiceFiscale(String nome, String cognome, int giornoNascita, int meseNascita, int annoNascita, char sesso, String comuneNascita){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.annoNascita = annoNascita;
+        this.meseNascita = meseNascita;
+        this.giornoNascita = giornoNascita;
+        this.comuneNascita = comuneNascita;
+        this.sesso = sesso;
     }
+
 }
