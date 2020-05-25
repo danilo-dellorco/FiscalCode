8705e260486460f30ce6f5c83e8bbf9e696027ec
MicheleSalvatori <misax.117@gmail.com>
2020-05-25 18:12:44 +0200

    creato pareser comuni

--- app/src/main/java/it/runningexamples/fiscalcode/Parser.java
+++ app/src/main/java/it/runningexamples/fiscalcode/Parser.java
@@ -1,7 +1,56 @@
 package it.runningexamples.fiscalcode;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+// TODO EXCEPTION
+
 public class Parser {
 
-    private static String DB_NAME = "comuni.csv";
+    private static Context context;
+    private static BufferedReader reader;
 
+    public Parser(Context context){
+        this.context = context;
+        parse();
+    }
+
+    private static List<Comune> parse() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(context.getAssets().open("comuni.csv")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<Comune> comuni = new ArrayList<>();
+        String line;
+        try {
+            line = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return comuni;
+        }
+        while (line != null) {
+            try {
+                StringTokenizer tokenizer = new StringTokenizer(line, ";");
+                String name = tokenizer.nextToken();
+                String province = tokenizer.nextToken();
+                String fiscalCode = tokenizer.nextToken();
+                Comune comune = new Comune(name, province, fiscalCode);
+                comuni.add(comune);
+                line = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return comuni;
+            }
+        }
+        return comuni;
+    }
 }
