573c3f1ac678dcd3823c4db1aa8da44dead997f2
MicheleSalvatori <misax.117@gmail.com>
2020-06-15 12:41:51 +0200

    switch tema fixed

--- app/src/main/java/it/runningexamples/fiscalcode/SettingsActivity.java
+++ app/src/main/java/it/runningexamples/fiscalcode/SettingsActivity.java
@@ -13,6 +13,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -21,27 +22,28 @@ import android.widget.Toast;
 
 import java.io.File;
 
-public class SettingsActivity extends AppCompatActivity implements Switch.OnCheckedChangeListener,View.OnClickListener{
+public class SettingsActivity extends AppCompatActivity implements Switch.OnCheckedChangeListener, View.OnClickListener {
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String THEME = "1";
     private static final int THEME_LIGHT = 0;
     private static final int THEME_DARK = 1;
+    public int lastTheme;
     PreferenceManager prefs;
-
+    public Switch swDarkMode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         prefs = new PreferenceManager(this);
         int theme = prefs.getTheme();
-        Switch swDarkMode = null;
+        swDarkMode = null;
 
-        if (theme == THEME_LIGHT){
+        if (theme == THEME_LIGHT) {
             setTheme(R.style.LightTheme);
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_settings);
             swDarkMode = findViewById(R.id.swDarkMode);
             swDarkMode.setChecked(false);
         }
-        if (theme == THEME_DARK){
+        if (theme == THEME_DARK) {
             setTheme(R.style.DarkTheme);
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_settings);
@@ -68,66 +70,39 @@ public class SettingsActivity extends AppCompatActivity implements Switch.OnChec
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+        lastTheme = prefs.getTheme();
+        Log.d("CodiceFiscale", String.valueOf(lastTheme));
         if (isChecked){
             prefs.setThemePref(THEME_DARK);
+            showDialogRestart(getString(R.string.changeThemeAlert), false);
         }
         else {
             prefs.setThemePref(THEME_LIGHT);
+            showDialogRestart(getString(R.string.changeThemeAlert), false);
         }
-        showDialogRestart("Per applicare il tema è necessario riavviare l'applicazione, vuoi farlo ora?", false);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btnIntro){
+        if (v.getId() == R.id.btnIntro) {
             prefs.setTempIntro(true);
             startActivity(new Intent(SettingsActivity.this, WelcomeActivity.class));
             finish();
         }
-        if (v.getId() == R.id.btnProfile){
+        if (v.getId() == R.id.btnProfile) {
             startActivity(new Intent(SettingsActivity.this, ProfileActivity.class));
             finish();
         }
-        if (v.getId() == R.id.btnDelete){
-            showDialogRestart("Per eliminare i dati è necessario riavviare l'applicazione, vuoi farlo ora?", true);
+        if (v.getId() == R.id.btnDelete) {
+            showDialogRestart(getString(R.string.deleteAlert), true);
         }
     }
 
-/*
-    Presente anche un metodo introdotto in API 19 ActivityManager.clearApplicationUserData()
-    ma va ad eliminare i dati ma killa anche le activity in run
- */
-
-    public void clearApplicationData() {
-        File cache = getCacheDir();
-        File appDir = new File(cache.getParent());
-        if (appDir.exists()) {
-            String[] children = appDir.list();
-            for (String s : children) {
-                if (!s.equals("lib")) {
-                    deleteDir(new File(appDir, s));
-
-                }
-            }
-        }
-//        restartApp(WelcomeActivity.class);
-    }
-    public static boolean deleteDir(File dir) {
-        if (dir != null && dir.isDirectory()) {
-            String[] children = dir.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-
-        return dir.delete();
+    public void deleteDB() {
+        AppDatabase.getInstance(getApplicationContext()).codiceFiscaleDAO().deleteAll();
     }
 
-    public void showDialogRestart(String textToShow, final Boolean clearApp){
+    public void showDialogRestart(String textToShow, final Boolean clearApp) {
         new AlertDialog.Builder(this)
                 .setMessage(textToShow)
                 .setIcon(android.R.drawable.ic_dialog_alert)
@@ -135,21 +110,28 @@ public class SettingsActivity extends AppCompatActivity implements Switch.OnChec
 
                     public void onClick(DialogInterface dialog, int whichButton) {
                         if (clearApp) {
-                            clearApplicationData();
-                        }else{
-                            restartApp(SettingsActivity.this);  //todo sistemare, è sbagliato
+                            deleteDB();
+                        } else {
+                            restartApp();
                         }
-                    }})
-                .setNegativeButton(android.R.string.no, null).show();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(!clearApp){
+                            prefs.setThemePref(lastTheme);
+//                            swDarkMode.setChecked(false);         sarebbe comodo ma richiama un'altra volta il listener e quindi addio
+                        }
+                    }
+                }).show();
     }
 
-    public void restartApp(Activity activity){
-        Intent mStartActivity = new Intent(SettingsActivity.this, activity.getClass());
-        int mPendingIntentId = 123456;
-        PendingIntent.getActivity(getApplicationContext(), mPendingIntentId,    mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
+    public void restartApp() {
+        Intent mStartActivity = new Intent(SettingsActivity.this, MainActivity.class);
+        int mPendingIntentId = 12345;
+        mStartActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent.getActivity(getApplicationContext(), mPendingIntentId, mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
         System.exit(0);
     }
-
-
-
 }
